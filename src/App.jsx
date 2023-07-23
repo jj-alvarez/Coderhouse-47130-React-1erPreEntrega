4c11fb800a8c6151c2fc2e91a3624dd034594699
juan jose alvarez <alvarez.juanjose@icloud.com>
2023-07-23 15:35:06 -0300

    leayout basico

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import './navbar.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className='sitio'>
+            <div className='header-area'>
+
+
+            </div>
+
+            <div className='main-area'>
+                <img src={reactLogo} className="logo-react" alt="React logo" />
+                <h1>La Biblioteca Escondida</h1>
+                <p className='proximamente'>Pagina en construccion. Subcribite para estar al tanto de nuestras novedades!</p>
+                <button className='btn-newsletter'>Newsletter</button>
+            </div>
+
+            <div className='footer-area'>
+                <p className="read-the-docs">
+                    La Biblioteca Escondida | Copyright 2023 | Todos los derechos reservados.
+                </p>
+            </div>
+        </div>
     </>
   )
 }
