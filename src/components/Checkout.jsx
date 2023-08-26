72afa2b89482f6e935766ef7cbd4f0b4962cb25f
juan jose alvarez <alvarez.juanjose@icloud.com>
2023-08-26 19:45:50 -0300

    checkout, ajustes varios

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -4,12 +4,43 @@ import { useRef } from 'react'
 import { useEffect} from 'react'
 import {Link} from 'react-router-dom'
 
+
 import './Checkout.css'
 import '../App.css'
 import HeaderArea from './headerArea';
 import FooterArea from './Footer';
 
 const Checkout = () => {
+
+    const [nombre, setNombre] = useState("");
+    const [apellido, setApellido] = useState("");
+    const [email, setEmail] = useState("");
+    const [okVisible, setOkVisible] = useState(false); 
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Compra efectuada por:", {nombre, apellido, email});
+        setOkVisible(true);
+    }
+
+    const handleNombre = (e) => {
+        setNombre(e.target.value);
+    }
+
+    const handleApellido = (e) => {
+        setApellido(e.target.value);
+    }
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const resetForm = () => {
+        setNombre("");
+        setApellido("");
+        setEmail("");
+    };
 
     return (
         <>
@@ -22,20 +53,42 @@ const Checkout = () => {
                         <p>Ingresa tus datos para finalizar la compra</p>
                     </div>
                     <div className='checkout-area'>
-                        <div className='renglon'>
-                            <p>Nombre</p><input type="text" />
-                        </div>
-                        <div className='renglon'>
-                            <p>Apellido</p><input type="text" />
-                        </div>
-                        <div className='renglon'>
-                            <p>e-mail</p><input type="text" />
-                        </div>
+                        <form onSubmit={handleSubmit}>
+                            <div className='renglon'>
+                                <p>Nombre</p><input type="text" onChange={handleNombre} value={nombre} placeholder='Juan'required/>
+                            </div>
+                            <div className='renglon'>
+                                <p>Apellido</p><input type="text" onChange={handleApellido} value={apellido} placeholder='Perez'required/>
+                            </div>
+                            <div className='renglon'>
+                                <p>e-mail</p><input type="email" onChange={handleEmail} value={email} placeholder='juan.perez@gmail.com'required method="post"/>
+                            </div>
+                            {okVisible ? (
+                                <Link to={`/Finalizar`}>
+                                    <button
+                                        onClick={() => {
+                                            resetForm();
+                                            setOkVisible(false);
+                                        }}
+                                        className="btn-volver333"
+                                    >
+                                        Finalizar
+                                    </button>
+                                </Link>
+                            ) : (
+                                <button type="submit" className="btn-volver33">
+                                    Comprar
+                                </button>
+                            )}
+                        </form>
                     </div>
-                    <div className='botones-footer'>
-                        <Link to={`/Cart`}><button className='btn-volver2'>Volver</button></Link>
-                        <Link to={`/Finalizar`}><button className="btn-volver3">Finalizar</button></Link>    
-                    </div>
+                    {!okVisible && (
+                        <div className='botones-footer'>
+                            <Link to={`/Cart`}>
+                                <button className='btn-volver22'>Volver a Carrito</button>
+                            </Link>
+                        </div>
+                    )}
                 </div>
 
                 <FooterArea />
